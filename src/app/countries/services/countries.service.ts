ac6dea97c7c035cc8b975e1c56c965eb6f07e340
mleandrocardenas <michaelcardenas199@gmail.com>
2024-02-04 15:30:10 -0500

    cambios en las pantallas para la visualizacion de la tabla

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
@@ -12,6 +12,22 @@ export class CountriesService {
 
     public searchCapital( term: string ):Observable<Country[]>  {
         return this.http.get<Country[]>(`${ this.apiUrl }/capital/${ term }`)
+        .pipe( 
+            catchError( () => of([]) )
+         );
+    }
+
+    public searchCountry( term: string ): Observable<Country[]> {
+        // const params = new HttpParams().set('fullText', true);
+        // return this.http.get<Country[]>(`${ this.apiUrl }/name/${ term }`, { params })
+        return this.http.get<Country[]>(`${ this.apiUrl }/name/${ term }`)
+        .pipe( 
+            catchError( () => of([]) )
+         );
+    }
+
+    public searchRegion( region: string ): Observable<Country[]> {
+        return this.http.get<Country[]>(`${ this.apiUrl }/region/${ region }`)
         .pipe( 
             catchError( () => of([]) )
          );
